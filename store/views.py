4b8b76176497f77aa180b70e387d8201849ebc0d
mateusz <gmateusz92@gmail.com>
2022-07-18 21:07:55 +0200

    update

--- store/views.py
+++ store/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from greatkart1.models import Category
+from carts.views import _cart_id
+from carts.models import CartItem
+
+from django.http import HttpResponse
 
 def store(request, category_slug=None):
     categories = None
@@ -18,6 +22,8 @@ def store(request, category_slug=None):
 def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug) #store - models category a nastepnie pobieramy slug z  getkard1 slug, ctaegory__slug daje dostep do slugu tego modelu
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
+
     except Exception as e:
         raise e
-    return render(request, 'store/product_detail.html', {'single_product': single_product})
+    return render(request, 'store/product_detail.html', {'single_product': single_product, 'in_cart': in_cart})
